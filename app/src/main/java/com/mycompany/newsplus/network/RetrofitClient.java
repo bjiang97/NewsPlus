99160bca1663c5c8c0a8e386fecfb4da27670b0a
Boyuan Jiang <jiangboyuan0221@gmail.com>
2021-08-31 21:32:33 -0700

    build retrofit connection

---
+++ app/src/main/java/com/mycompany/newsplus/network/RetrofitClient.java
@@ -0,0 +1,53 @@
+package com.mycompany.newsplus.network;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static String API_KEY;
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+
+    public static Retrofit newInstance() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HeaderInterceptor())
+                .build();
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    private static class HeaderInterceptor implements Interceptor {
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+
+            //        read config file by using input stream
+            InputStream inputStream = RetrofitClient.class.getClassLoader().getResourceAsStream(propFileName);
+            prop.load(inputStream);
+            API_KEY = prop.getProperty("newsapi_key");
+
+            Request original = chain.request();
+            Request request = original
+                    .newBuilder()
+                    .header("X-Api-Key", API_KEY)
+                    .build();
+            return chain.proceed(request);
+        }
+    }
+
+
+}
